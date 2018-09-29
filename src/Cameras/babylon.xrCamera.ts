44c4252711d2bb22463ad8604851c1d8dc34db44
Trevor Baron <trbaron@microsoft.com>
2018-09-28 17:55:16 -0700

    enter/exit

--- src/Cameras/babylon.xrCamera.ts
+++ src/Cameras/babylon.xrCamera.ts
@@ -46,9 +46,9 @@ module BABYLON {
             }
         }
 
-        public updateFromFrame(xrFrame:any, frameOfRef:any, session:any){
+        public updateFromFrame(xrFrame:any, xrSessionManager:XRSessionManager){
             if(xrFrame && xrFrame.getDevicePose){
-                var pose = xrFrame.getDevicePose(frameOfRef);
+                var pose = xrFrame.getDevicePose(xrSessionManager.frameOfReference);
                 if(pose && pose.poseModelMatrix){
                     // Update the parent cameras matrix
                     var matrix = BABYLON.Matrix.Identity()
@@ -69,7 +69,7 @@ module BABYLON {
                     }else{                        
                         xrFrame.views.forEach((view:any, i:number)=>{
                             // TODO: How could we handle multiple headsets connected at the same time in the future?
-                            var viewport  = session.baseLayer.getViewport(xrFrame.views[i])
+                            var viewport  = xrSessionManager._xrSession.baseLayer.getViewport(xrFrame.views[i])
     
                             BABYLON.Matrix.FromFloat32ArrayToRefScaled(pose.getViewMatrix(xrFrame.views[i]), 0, 1, this.rigCameras[i]._computedViewMatrix)                                    
                             BABYLON.Matrix.FromFloat32ArrayToRefScaled(xrFrame.views[i].projectionMatrix, 0, 1, this.rigCameras[i]._projectionMatrix)
@@ -84,21 +84,82 @@ module BABYLON {
 
                             // TODO is the parenting working right? I expect to need to set view matrix of children to this divided by parent
                             // TODO figure out how to set viewport using ratio like other places
-                            var width = session.baseLayer.framebufferWidth
-                            var height = session.baseLayer.framebufferHeight
+                            var width = xrSessionManager._xrSession.baseLayer.framebufferWidth;
+                            var height = xrSessionManager._xrSession.baseLayer.framebufferHeight;
                             this.rigCameras[i].viewport.width = viewport.width/width;
                             this.rigCameras[i].viewport.height = viewport.height/height;
                             this.rigCameras[i].viewport.x = viewport.x/width;
                             this.rigCameras[i].viewport.y = viewport.y/height;
+
+                            // Set cameras to render to the sessions render target
+                            this.rigCameras[i].customDefaultRenderTarget = xrSessionManager.sessionRenderTargetTexture;
                         })
                     }
                 }
-                
             }
         }
     }
 
+    export class XRSessionManagerEnterXROptions {
+        outputCanvas?:HTMLCanvasElement
+    }
+
     export class XRSessionManager {
+        private _xrNavigator:any;
+        private _xrDevice:any;
+        public _xrSession:any;
+        private _outputContext:any;
+        private _scene:Scene;
+        public frameOfReference:any;
+        public sessionRenderTargetTexture:RenderTargetTexture;
+        constructor(){}
+
+        initialize(scene:BABYLON.Scene):Promise<any>{
+            this._scene = scene
+
+            // Check if the browser supports webXR
+            this._xrNavigator = navigator;
+            if(!this._xrNavigator.xr){
+                return Promise.reject("webXR not supported by this browser");
+            }
+
+            // Request the webXR device
+            return this._xrNavigator.xr.requestDevice().then((device:any)=>{
+                this._xrDevice = device;
+                return (<any>scene.getEngine()._gl).setCompatibleXRDevice(this._xrDevice)
+            })
+        }
+
+        enterXR(options:XRSessionManagerEnterXROptions):Promise<any>{
+            var arMode = true;
+
+            // initialize session
+            this._outputContext = options.outputCanvas!.getContext('xrpresent')
+            return this._xrDevice.requestSession({immersive: !arMode, environmentIntegration: arMode, outputContext: this._outputContext}).then((session:any)=>{
+                this._xrSession = session;
+                this._xrSession.baseLayer = new XRWebGLLayer(this._xrSession, this._scene.getEngine()._gl);
+                return this._xrSession.requestFrameOfReference('eye-level');
+            }).then((frameOfRef:any)=>{
+                this.frameOfReference = frameOfRef;
+
+                // Tell the engine's render loop to be driven by the xr session's refresh rate and provide xr pose information
+                this._scene.getEngine().customAnimationFrameRequester = this._xrSession
+
+                // Create render target texture from xr's webgl render target
+                this.sessionRenderTargetTexture = XRSessionManager.CreateRenderTargetTextureFromSession(this._xrSession, this._scene);
+                this._outputContext.width = this.sessionRenderTargetTexture.getRenderWidth();
+                this._outputContext.height =this.sessionRenderTargetTexture.getRenderHeight();
+            })
+        }
+
+        exitXR(){
+            this._scene.getEngine().customAnimationFrameRequester = null;
+            this._xrSession.end()
+            
+            // Need to restart render loop as after calling session.end the last request for new frame will never call callback
+            this._scene.getEngine()._renderLoop(0)
+        }
+
         static CreateRenderTargetTextureFromSession(session:any, scene:BABYLON.Scene){
             // Create internal texture
             var internalTexture = new BABYLON.InternalTexture(scene.getEngine(), BABYLON.InternalTexture.DATASOURCE_UNKNOWN, true)
@@ -111,10 +172,6 @@ module BABYLON {
             renderTargetTexture._texture = internalTexture;
 
             return renderTargetTexture;
-        }
-
-        constructor(){
-            
         }
     }
 }
